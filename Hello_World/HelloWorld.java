a699e9866491763cbb433195c0ab64d9d5db8d0b
Chanto Umana <umanav@amazon.com>
2018-04-23 11:30:17 -0600

    added first assessment

---
+++ Hello_World/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("My name is Victor Chanto");
+        System.out.println("I am 24 years old");
+        System.out.println("My Hometown is San Marcos, San Jose, Costa Rica");
+    }
+}
